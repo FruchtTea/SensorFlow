5723b3fe47d94ff676367c94ae33582dfd73ffc6
Max Plabst <mbplabst05@gmail.com>
2023-06-27 00:09:27 +0200

    Design and Renaming changes

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from flask import Flask, render_template
-import folium
-import time
-import threading
+import folium, time, threading
 import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
 
 lat = 48.13720
-long = 11.51000 
-temp = 172.5
+long = 11.51000
+temp = 0
 humid = 0
+
 
 @app.route("/")
 def render_main_page():
     return render_template("index.html")
 
-@app.route("/geo")
-def render_karte():
 
-    map = folium.Map(
-        location=[lat, long],
-        zoom_start=12,
-        tiles="Stamen Terrain"
-        )
-    
+@app.route("/geolocator")
+def render_geolocator_page():
+    return render_template("geolocator.html")
+
+
+@app.route("/render-geolocation-page")
+def render_karte():
+    map = folium.Map(location=[lat, long], zoom_start=12, tiles="Stamen Terrain")
+
     folium.Marker(
         location=[lat, long],
         popup="<b>Dein Standord!</b>",
@@ -33,20 +33,23 @@ def render_karte():
 
     return map._repr_html_()
 
-@app.route("/temperatur")
+
+@app.route("/raumklima")
 def render_temperatur_page():
     global temp
     temp = temp + 1
-    return render_template("temperatur.html", show_temperature=temp)
+    return render_template("raumklima.html", show_temperature=temp)
+
 
 def startserver():
     app.run()
 
+
 if __name__ == "__main__":
     t1 = threading.Thread(target=startserver)
     t1.start()
-    
-    
+
+
 ############### HIER STARTET MQTT LOGIK ###############
 gruppenname = "sensorik"
 id = "IoTinformatikschulbuch"
@@ -63,10 +66,12 @@ mqtt_client.connect("test.mosquitto.org")
 mqtt_client.loop_start()
 print("MQTT connected")
 
+
 # Methode wird aufgerufen, sobald eine neue Nachricht empfangen wurde
 def verarbeite_telemetry(client, nutzerdaten, nachricht):
     payload = nachricht.payload.decode()
     print("Nachricht empfangen: ", payload)
+
 
 # Abonniert die oben gegebene Topic
 mqtt_client.subscribe(client_telemetry_topic)
@@ -77,10 +82,6 @@ mqtt_client.on_message = verarbeite_telemetry
 while True:
     try:
         # Warte eine halbe Sekunde für die nächste Messung
-        time.sleep(.1)
+        time.sleep(0.1)
     except IOError:
-        print ("Error")
-
-
-
-     
+        print("Error")
